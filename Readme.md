098e6669e8bcf4eb7eb96717df00645762a727ed
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2023-11-20 11:55:17 -0300

    Estrutura do projeto em flask

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Comandos
+
+- pip freeze > requirementes.tx
---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask.cli import FlaskGroup
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+cli = FlaskGroup(app)
+
+migrate = Migrate()
+migrate.init_app(app, db)
+
+
+from app.controllers import default 
